6806269ae84b7ed5e2ddccaf551d4d0ec9461608
DeepInTheFramework <132104261+DeepInTheFramework@users.noreply.github.com>
2024-05-03 19:41:36 +0200

    Responsive pour toute taille d'écran (sauf mobile) + ajouts du social + animation du menu socials et animation + animation du menu header
    Responsive.
    Ajout du menu social à droite.
    
    Animation du menu header grâce à une fonction codé en js s'activant en mouseEnter et mouseLeave

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import MangaHighlights from './MangaHighlights';
 import logo from './img/header/logo.png';
 import creatorBanner from './img/creators-banner.jpg';
 import Footer from './Footer'
+import Socials from './Socials';
 
 
 function App() {
@@ -28,14 +29,82 @@ function App() {
     if (targetDivRef.current && targetDivRef.current.offsetTop !== undefined) {
       console.log("Cible existante")
         // Défilement vers la position de la div cible
-        document.querySelector('body').scrollTo(0,0);
+        window.location.href = "/#siteStart";
     } 
 };
   
+      function YellowHighlightOnEnter(divName) {
+            const yellowDiv = document.getElementById('yellowHightlightID')
+            console.log (yellowDiv)
+        switch (divName) {
+
+
+          case 'recent' :
+            const recentDiv = document.querySelector('.recent'); 
+            const recentRect = recentDiv.getBoundingClientRect();
+            yellowDiv.style.top = 0;
+            yellowDiv.style.left = `${recentRect.left-10}px`;
+            yellowDiv.style.width = `${recentRect.width+20}px`;
+            break;
+
+
+            case 'enVedette' :
+              const enVedetteDiv = document.querySelector('.enVedette'); 
+              const enVedetteRect = enVedetteDiv.getBoundingClientRect();
+              yellowDiv.style.top = 0;
+              yellowDiv.style.left = `${enVedetteRect.left-10}px`;
+              yellowDiv.style.width = `${enVedetteRect.width+20}px`;
+              break;
+
+              case 'listeDesMangas' :
+                const listeDesMangasDiv = document.querySelector('.listeDesMangas'); 
+                const listeDesMangasRect = listeDesMangasDiv.getBoundingClientRect();
+                yellowDiv.style.top = 0;
+                yellowDiv.style.left = `${listeDesMangasRect.left-10}px`;
+                yellowDiv.style.width = `${listeDesMangasRect.width+20}px`;
+                break;  
+
+            case 'creators' :
+              const creatorsDiv = document.querySelector('.creators'); 
+              const creatorsRect = creatorsDiv.getBoundingClientRect();
+              yellowDiv.style.top = 0;
+              yellowDiv.style.left = `${creatorsRect.left-10}px`;
+              yellowDiv.style.width = `${creatorsRect.width+20}px`;
+              break;
+
+              case 'favoris' :
+              const favorisDiv = document.querySelector('.favoris'); 
+              const favorisRect = favorisDiv.getBoundingClientRect();
+              yellowDiv.style.top = 0;
+              yellowDiv.style.left = `${favorisRect.left-10}px`;
+              yellowDiv.style.width = `${favorisRect.width+20}px`;
+              break;
+
+              case 'aProposDeNous' :
+                const aProposDeNousDiv = document.querySelector('.aProposDeNous'); 
+                const aProposDeNousRect = aProposDeNousDiv.getBoundingClientRect();
+                yellowDiv.style.top = 0;
+                yellowDiv.style.left = `${aProposDeNousRect.left-10}px`;
+                yellowDiv.style.width = `${aProposDeNousRect.width+20}px`;
+                break;
+        }
+
+
+
+      }
+
+      function YellowHighlightOnLeave() {
+        const yellowDiv= document.getElementById('yellowHightlightID')
+          yellowDiv.style.top = '-5px';
+          yellowDiv.style.width = '10px';
+
+  }
+
+        
   return (
 
     
-    
+   
     <div className="App">
       
       <header className="header">
@@ -44,13 +113,37 @@ function App() {
           </div>
 
           <div className='menu' id='siteStart'>
-              <ul>
-                <li>RÉCENT</li>
-                <li>EN VEDETTE</li>
-                <li>LISTE DES MANGAS</li>
-                <li>CREATORS</li>
-                <li>FAVORIS</li>
-                <li>À PROPOS DE NOUS</li>
+              <ul  style={{ listStyleType: 'none' }}>
+                <li className='recent'
+                onMouseEnter={ ()=> YellowHighlightOnEnter('recent')}
+                onMouseLeave={() => YellowHighlightOnLeave()}
+                >RÉCENT</li>
+                <li className='enVedette'
+                                                onMouseEnter={()=> YellowHighlightOnEnter('enVedette')}
+                                                onMouseLeave={() => YellowHighlightOnLeave()}
+                >EN VEDETTE</li>
+
+                
+                <li className='listeDesMangas'
+                                onMouseEnter={()=> YellowHighlightOnEnter('listeDesMangas')}
+                                onMouseLeave={() => YellowHighlightOnLeave()}
+                >LISTE DES MANGAS</li>
+                <li className='creators' 
+                onMouseEnter={ ()=> YellowHighlightOnEnter('creators')}
+                onMouseLeave={() => YellowHighlightOnLeave()}
+                >
+                  
+                  CREATORS
+                </li>
+                <li className='favoris'
+                onMouseEnter={ ()=> YellowHighlightOnEnter('favoris')}
+                onMouseLeave={() => YellowHighlightOnLeave()}                
+                >FAVORIS</li>
+
+                <li className='hideOnSmallScreen aProposDeNous'
+                                                onMouseEnter={()=> YellowHighlightOnEnter('aProposDeNous')}
+                                                onMouseLeave={() => YellowHighlightOnLeave()}
+                >À PROPOS DE NOUS</li>
               </ul>
           </div>
 
@@ -59,6 +152,9 @@ function App() {
               placeholder='Recherche par titre ou par auteur'
             />
           </div>
+
+          <div className='redHightlight'></div>
+          <div className='yellowHightlight' id='yellowHightlightID'></div>
       </header>
       <Annoucement />
 
@@ -67,6 +163,7 @@ function App() {
             <MangaScreen sendRef={ref => (targetDivRef.current = ref)} />
         </div>
         <div className='rightContainer'>
+          <Socials />
           <img src={creatorBanner} alt='banner about creators' style={{ borderTopLeftRadius : '10px', borderBottomLeftRadius : '10px'}}/>
           <MangaHighlights />
         </div>
---
+++ src/Socials.js
@@ -0,0 +1,19 @@
+import './css/Socials.css'
+
+export default function Socials () {
+            return (
+                <div className="socialsContainer">
+                    <div className="discord">
+                        <span>Discord</span>
+                    </div>
+
+                    <div className="twitter">
+                    <span>Twitter</span>
+                    </div>
+
+                    <div className="facebook">
+                    <span>Facebook</span>
+                    </div>
+                </div>
+            )
+}
